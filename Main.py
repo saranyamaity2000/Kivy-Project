5324dd4b4fd188c4fffb0601822ffbd3d28da0ff
saranyamaity <saranyamaity2000@gmail.com>
2021-07-09 00:12:29 +0530

    project day 1

---
+++ Main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class MainWidget(Widget):
+    pass
+
+
+class TheLabApp(App):
+    pass
+
+
+TheLabApp().run()
