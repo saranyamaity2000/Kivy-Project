6cb8e0d64912cf4d311c4fc125dc2cbfe53f87db
saranyamaity <saranyamaity2000@gmail.com>
2021-07-09 18:33:34 +0530

    Modified Main.py for next learning

--- Main.py
+++ Main.py
@@ -11,17 +11,9 @@ from kivy.uix.widget import Widget
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)  # this above two default constructer line !
-        inc = 0.1
-        pos = 1
-        area = 1
-        while area > 0:  # just to test how many buttons fit inside !
-            # doesn't matter if i do >0.5 or >1 the total shown is 5 only !
-            b = Button(text=str(pos), size_hint=(None, None), size=(dp(100), dp(100)))
-            area -= (inc**2)
-            if area < 0:
-                break
-            inc += 0.1
-            pos += 1
+        for i in range(1,101): # 100 buttons
+            b = Button(text=str(i), size_hint=(None, None), size=(dp(100), dp(100)))
+
             self.add_widget(b)
 
 
